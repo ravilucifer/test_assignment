2d832eb46f9d21db7ea1e652523fd534ac0d9af2
ravi.sharma@entitcs.com <ravi.sharma@entitcs.com>
2023-08-29 18:35:54 +0530

    added bottomnavigation

--- app/src/main/java/com/example/test_assignment/MainActivity.java
+++ app/src/main/java/com/example/test_assignment/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.test_assignment;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    BottomNavigationView bottomBarLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
     }
 }
